6c964ab00834a568b49efafecf2d0242c479e23d
s1F102301156 <s1F102301156@iniad.org>
2023-09-15 23:50:30 +0900

    追加機能
    おかれている石の数を数えて表示する機能、どっちのターン化を表示する機能を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,12 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+
+  const [blackCount, setBlackCount] = useState(2);
+  const [whiteCount, setWhiteCount] = useState(2);
+
+  const [turnJudge, setTurnJudge] = useState("black");
+
   const directions = [
     [-1, 1],
     [0, 1],
@@ -22,9 +28,11 @@ const Home = () => {
     [-1, -1],
     [-1, 0],
   ];
+
   const [turnColor, setTurnColor] = useState(1);
   const clickCell = (x: number, y: number) => {
     const newBoard = JSON.parse(JSON.stringify(board));
+    let stonePlaced = false;
     if (newBoard[y][x] === 0) {
       for (const canPlace of directions) {
         for(let distance = 1; distance < 8; distance++) {
@@ -42,6 +50,7 @@ const Home = () => {
                 }
                 setBoard(newBoard);
                 setTurnColor(3 - turnColor);
+                stonePlaced = true;
               }
               break;
             } else if (newBoard[y + canPlace[0] * distance][x + canPlace[1] * distance] === 3 - turnColor) {
@@ -51,9 +60,37 @@ const Home = () => {
         }
       }
     }
+
+    if (stonePlaced) {
+      let countStoneBlack = 0;
+      let countStoneWhite = 0;
+      for (let wide = 0; wide < 8; wide++) {
+        for (let warp = 0; warp < 8; warp++) {
+          if (newBoard[wide][warp] === 1) {
+            countStoneBlack++;
+          } else if (newBoard[wide][warp] === 2) {
+            countStoneWhite++;
+          }
+        }
+      }
+      setBlackCount(countStoneBlack);
+      setWhiteCount(countStoneWhite);
+
+      if (turnColor === 2) {
+        setTurnJudge("Black"); 
+      } else if (turnColor === 1) {
+        setTurnJudge("White");
+      }
+    }
   };
   return (
     <div className={styles.container}>
+      <h2 className={styles.now}>
+        Black: {blackCount} / White: {whiteCount}
+      </h2>
+      <h2 className={styles.now}>
+        Turn: {turnJudge}
+      </h2>
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((color, x) => (
